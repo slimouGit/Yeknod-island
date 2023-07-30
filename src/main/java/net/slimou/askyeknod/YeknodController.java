2f8ee02bd78bacf3df6d6d1ea191b8027a9ba65d
box@slimou.net <box@slimou.net>
2023-07-30 10:21:57 +0200

    yeknod implement input/output

--- src/main/java/net/slimou/askyeknod/YeknodController.java
+++ src/main/java/net/slimou/askyeknod/YeknodController.java
@@ -1,7 +1,9 @@
 package net.slimou.askyeknod;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class YeknodController {
@@ -14,6 +16,21 @@ public class YeknodController {
 
     @GetMapping(path = "/home")
     public String homeOfTheYeknod(){
+        return "index";
+    }
+
+    @GetMapping
+    public String showForm(Model model) {
+        model.addAttribute("text", ""); // Initialisieren des Textfelds mit leerem Inhalt
+        return "index";
+    }
+
+    @PostMapping
+    public String processForm(String text, Model model) {
+        model.addAttribute("text", text); // Das eingegebene Textfeld wird im Model gespeichert
+        String response = this.yeknodService.startRequest(text);
+        model.addAttribute("response", response); // Die Response des Service wird im Model gespeichert
+
         return "index";
     }
 }
--- src/main/java/net/slimou/askyeknod/YeknodService.java
+++ src/main/java/net/slimou/askyeknod/YeknodService.java
@@ -2,6 +2,15 @@ package net.slimou.askyeknod;
 
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class YeknodService {
+
+    private static final Logger logger = Logger.getLogger(YeknodService.class.getName());
+
+    public String startRequest(String text) {
+        logger.info(String.format("request %s", text));
+        return text;
+    }
 }
